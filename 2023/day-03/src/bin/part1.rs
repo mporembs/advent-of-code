ca5b44ea8be03bd4dfad5da92e2c5cf83b86d7a7
Matt Porembski <matt@poremb.ski>
2023-12-04 14:35:35 -0700

    Solved along with video

--- 2023/day-03/src/bin/part1.rs
+++ 2023/day-03/src/bin/part1.rs
@@ -1,60 +1,141 @@
+use core::num;
+use itertools::Itertools;
+use std::collections::BTreeMap;
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
     dbg!(output);
 }
+#[derive(Debug)]
+enum CharType {
+    Symbol(char),
+    Empty,
+    Number(u32),
+}
+#[derive(Ord, Eq, PartialOrd, PartialEq, Debug, Clone, Copy)]
+struct GridSpot {
+    y_index: i32,
+    x_index: i32,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
 
 fn part1(input: &str) -> String {
-    let mut result = 0;
-    let lines_vec: Vec<&str> = input.lines().map(|line| line.trim()).collect();
+    let mut total = 0;
 
-    let symbols = input.lines().map(|line| {
-        line.trim()
-            .match_indices(|character: char| {
-                if character == '.' {
-                    return false;
-                } else if character.is_alphanumeric() {
-                    return false;
+    let grid = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y_index, line)| {
+            line.trim()
+                .chars()
+                .enumerate()
+                .map(move |(x_index, character)| {
+                    (
+                        GridSpot {
+                            x_index: x_index as i32,
+                            y_index: y_index as i32,
+                        },
+                        match character {
+                            '.' => CharType::Empty,
+                            c if c.is_ascii_digit() => {
+                                CharType::Number(c.to_digit(10).expect("that it is a number."))
+                            }
+                            c => CharType::Symbol(c),
+                        },
+                    )
+                })
+        })
+        .collect::<BTreeMap<GridSpot, CharType>>();
+
+    let mut numbers: Vec<Vec<(GridSpot, u32)>> = vec![];
+    for (grid_spot, character) in grid.iter() {
+        if let CharType::Number(num) = character {
+            match numbers.iter().last() {
+                Some(v) => {
+                    let last_num = v.iter().last();
+                    match last_num {
+                        Some((g, _)) => {
+                            if g.x_index + 1 == grid_spot.x_index {
+                                let last = numbers.iter_mut().last().expect("should exist");
+                                last.push((*grid_spot, *num));
+                            } else {
+                                numbers.push(vec![(*grid_spot, *num)]);
+                            }
+                        }
+                        None => unimplemented!("should't happen"),
+                    }
                 }
-                true
-            })
-            .collect::<Vec<_>>()
-    });
-
-    for (line_idx, line) in symbols.enumerate() {
-        let mut idx: usize = 0;
-
-        for symbol in &line {
-            let left_number: u32;
-            if idx == 0 {
-                left_number = lines_vec[line_idx]
-                    .get(0..symbol.0)
-                    .expect("to be a string")
-                    .split(pat)
-                    .chars()
-                    .filter_map(|character| character.to_digit(10))
-                    .map(|digit| digit.to_string())
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .unwrap_or_default();
-            } else {
-                left_number = lines_vec[line_idx]
-                    .get(line[idx - 1].0..symbol.0)
-                    .expect("to be a string")
-                    .chars()
-                    .filter_map(|character| character.to_digit(10))
-                    .map(|digit| digit.to_string())
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .unwrap_or_default();
+                None => numbers.push(vec![(*grid_spot, *num)]),
             }
-            result += left_number;
-            println!("Line {line_idx}: {left_number} before {}", symbol.1);
-            idx += 1;
+            // println!("{:?}", GridSpot)
         }
     }
+    // dbg!(numbers);
 
-    result.to_string()
+    for num_list in numbers {
+        // (x,y) relative
+        let positions: [Coord; 8] = [
+            Coord { x: 1, y: 0 },
+            Coord { x: 1, y: -1 },
+            Coord { x: 0, y: -1 },
+            Coord { x: -1, y: -1 },
+            Coord { x: -1, y: 0 },
+            Coord { x: -1, y: 1 },
+            Coord { x: 0, y: 1 },
+            Coord { x: 1, y: 1 },
+        ];
+        let num_positions: Vec<Coord> = num_list
+            .iter()
+            .map(|(gridspot, _)| Coord {
+                x: gridspot.x_index as i32,
+                y: gridspot.y_index as i32,
+            })
+            .collect();
+        let pos_to_check: Vec<Coord> = num_list
+            .iter()
+            .flat_map(|(pos, _)| {
+                positions.iter().map(|outer_pos| Coord {
+                    x: outer_pos.x + pos.x_index as i32,
+                    y: outer_pos.y + pos.y_index as i32,
+                })
+            })
+            .unique()
+            .filter(|num| !num_positions.contains(num))
+            .collect::<Vec<Coord>>();
+
+        // dbg!(pos_to_check.len(), pos_to_check);
+        let is_part_number = pos_to_check.iter().any(|pos| {
+            let pos_grid_spot = GridSpot {
+                x_index: pos.x,
+                y_index: pos.y,
+            };
+            let value = grid.get(&pos_grid_spot);
+            if let Some(CharType::Symbol(_)) = value {
+                true
+            } else {
+                false
+            }
+        });
+
+        if is_part_number {
+            total += num_list
+                .iter()
+                .map(|(_, num)| num.to_string())
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap();
+        }
+
+        // dbg!(is_part_number, n);
+    }
+
+    total.to_string()
 }
 
 #[cfg(test)]
