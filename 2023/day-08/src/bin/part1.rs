0ed8fe6c3fa0ca876b8939fdb0a7591b4fb1ff1b
Matt Porembski <matt@poremb.ski>
2023-12-08 08:11:51 -0700

    Setup 2023 day-08

---
+++ 2023/day-08/src/bin/part1.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    "0".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1(
+            "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)",
+        );
+        assert_eq!(result, "6".to_string());
+    }
+}
