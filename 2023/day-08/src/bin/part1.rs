80dbda1905564f93cb4a84cc5c8217d3a6978466
Matt Porembski <matt@poremb.ski>
2023-12-08 20:42:44 -0700

    Solved
    + Needs refactor

--- 2023/day-08/src/bin/part1.rs
+++ 2023/day-08/src/bin/part1.rs
@@ -1,11 +1,68 @@
+use std::{char, collections::HashMap};
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
     dbg!(output);
 }
 
+#[derive(Debug)]
+struct Branches {
+    left: String,
+    right: String,
+}
+
 fn part1(input: &str) -> String {
-    "0".to_string()
+    let mut split_input = input.lines().filter(|line| !line.eq(&""));
+    let route = split_input.next().unwrap().chars().collect::<Vec<char>>();
+
+    let node_map: HashMap<&str, Branches> = split_input
+        .clone()
+        .map(|node_line| {
+            let mut iter = node_line.split(' ');
+
+            (
+                iter.next().unwrap(),
+                Branches {
+                    left: iter
+                        .nth(1)
+                        .unwrap()
+                        .chars()
+                        .filter(|character| character.is_ascii_alphabetic())
+                        .collect::<String>(),
+                    right: iter
+                        .next()
+                        .unwrap()
+                        .chars()
+                        .filter(|character| character.is_ascii_alphabetic())
+                        .collect::<String>(),
+                },
+            )
+        })
+        .collect();
+
+    let mut current_node: &str = "AAA";
+    let mut steps = 0;
+
+    while !(current_node == "ZZZ") {
+        for turn in &route {
+            match turn {
+                'L' => {
+                    current_node = &node_map.get(current_node).unwrap().left;
+                    steps += 1
+                }
+                'R' => {
+                    current_node = &node_map.get(current_node).unwrap().right;
+                    steps += 1
+                }
+                _ => break,
+            }
+        }
+    }
+
+    // dbg!(route);
+    // dbg!(node_map);
+    steps.to_string()
 }
 
 #[cfg(test)]
