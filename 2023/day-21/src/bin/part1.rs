21c4d4614010c780a8937aeb05b0af80e5cf57f8
Matt Porembski <matt@poremb.ski>
2024-01-15 15:55:09 -0700

    Refactored

--- 2023/day-21/src/bin/part1.rs
+++ 2023/day-21/src/bin/part1.rs
@@ -1,6 +1,5 @@
-use std::collections::{HashMap, HashSet};
-
 use glam::IVec2;
+use std::{collections::HashSet, iter::successors};
 
 fn main() {
     let input = include_str!("./input1.txt");
@@ -9,86 +8,59 @@ fn main() {
 }
 
 fn part1(input: &'static str) -> String {
-    let grid = to_grid(input);
-    let start = grid
-        .iter()
-        .filter(|(_, plot)| match plot {
-            PlotType::Garden => false,
-            PlotType::Rock => false,
-            PlotType::Start => true,
-        })
-        .take(1)
-        .next()
-        .unwrap();
-    let mut reachable: HashSet<IVec2> = HashSet::from([start.0.to_owned()]);
+    let skip_num = 6;
+    let (start, grid) = to_grid(input);
+    // let starting_map: HashSet<IVec2> = HashSet::from([start]);
 
-    for _ in 0..64 {
-        let mut new_reachable: HashSet<IVec2> = HashSet::new();
-        reachable.drain().for_each(|position| {
-            let possible: Vec<IVec2> = Vec::from([
-                IVec2 {
-                    x: position.x.clone(),
-                    y: position.y.clone() + 1i32,
-                },
-                IVec2 {
-                    x: position.x.clone(),
-                    y: position.y.clone() - 1i32,
-                },
-                IVec2 {
-                    x: position.x.clone() - 1i32,
-                    y: position.y.clone(),
-                },
-                IVec2 {
-                    x: position.x.clone() + 1i32,
-                    y: position.y.clone(),
-                },
-            ]);
-            possible
-                .iter()
-                .for_each(|new_poss| match grid.get(new_poss).unwrap() {
-                    PlotType::Garden => {
-                        new_reachable.insert(*new_poss);
-                    }
-                    PlotType::Rock => (),
-                    PlotType::Start => {
-                        new_reachable.insert(*new_poss);
-                    }
-                });
-        });
-        reachable = new_reachable;
-    }
-    // dbg!(grid);
-    reachable.len().to_string()
+    let last_set_len = get_reachable_at(skip_num, &grid, &start);
+
+    last_set_len.to_string()
 }
 
-fn to_grid(input: &str) -> HashMap<IVec2, PlotType> {
-    input
+fn get_reachable_at(steps: usize, grid: &HashSet<IVec2>, start: &IVec2) -> usize {
+    let starting_map: HashSet<IVec2> = HashSet::from([start.to_owned()]);
+
+    successors(Some(starting_map), |reachable_positions| {
+        let mut new_reachable: HashSet<IVec2> = HashSet::new();
+
+        for pos in reachable_positions.into_iter() {
+            [IVec2::X, IVec2::NEG_X, IVec2::NEG_Y, IVec2::Y]
+                .into_iter()
+                .filter_map(|step| {
+                    let new_pos = *pos + step;
+                    grid.contains(&new_pos).then_some(new_pos)
+                })
+                .for_each(|new| {
+                    new_reachable.insert(new);
+                })
+        }
+        Some(new_reachable)
+    })
+    .skip(steps)
+    .next()
+    .unwrap()
+    .len()
+}
+
+fn to_grid(input: &str) -> (IVec2, HashSet<IVec2>) {
+    let start = IVec2 {
+        x: (input.lines().next().unwrap().chars().count() / 2) as i32,
+        y: (input.lines().count() / 2) as i32,
+    };
+    let grid = input
         .lines()
         .enumerate()
         .flat_map(|(y_index, line)| {
-            line.chars().enumerate().map(move |(x_index, ch)| {
-                (
-                    IVec2 {
-                        x: x_index as i32,
-                        y: y_index as i32,
-                    },
-                    match ch {
-                        '.' => PlotType::Garden,
-                        '#' => PlotType::Rock,
-                        'S' => PlotType::Start,
-                        _ => unreachable!(),
-                    },
-                )
-            })
+            line.chars()
+                .enumerate()
+                .filter_map(move |(x_index, ch)| match ch {
+                    '.' | 'S' => Some(IVec2::new(x_index as i32, y_index as i32)),
+                    _ => None,
+                })
         })
-        .collect::<HashMap<IVec2, PlotType>>()
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum PlotType {
-    Garden,
-    Rock,
-    Start,
+        .collect::<HashSet<IVec2>>();
+    // dbg!(&start);
+    (start, grid)
 }
 
 #[cfg(test)]
