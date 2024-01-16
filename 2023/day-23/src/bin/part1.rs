b252329a2192c781cb9c567d1d0fdcd8f7c3c60c
Matt Porembski <matt@poremb.ski>
2024-01-15 18:01:26 -0700

    Refactor to use petgraph all_simple_paths
    -Passes test now

--- 2023/day-23/src/bin/part1.rs
+++ 2023/day-23/src/bin/part1.rs
@@ -1,20 +1,7 @@
 use glam::IVec2;
 use itertools::Itertools;
-use petgraph::algo::Measure;
-use petgraph::visit::{EdgeRef, IntoEdges, Visitable};
+use petgraph::algo::all_simple_paths;
 use petgraph::{stable_graph::NodeIndex, Graph};
-use std::cmp::Ordering;
-use std::collections::hash_map::Entry::{Occupied, Vacant};
-use std::collections::{BinaryHeap, HashMap};
-use std::hash::Hash;
-
-//
-//
-//
-//  DOESN'T PASS TEST BUT GOT STAR ???
-//
-//
-//
 
 fn main() {
     let input = include_str!("./input1.txt");
@@ -234,91 +221,32 @@ fn part1(input: &'static str) -> String {
                 }
             })
     });
-
-    let count = longest_path(
+    let all_paths = all_simple_paths::<Vec<_>, &Graph<IVec2, u32>>(
         &graph,
         start.unwrap(),
-        |node_idx| node_idx == goal.unwrap(),
-        |edge| *edge.weight(),
-        |_| 0,
+        goal.unwrap(),
+        1usize,
+        None,
     )
-    .unwrap();
+    .collect_vec();
 
-    dbg!(count);
-    // dbg!(graph, start, goal);
-    count.to_string()
-}
+    let longest = all_paths
+        .iter()
+        .map(|path| {
+            path.iter()
+                .tuple_windows::<(_, _)>()
+                .fold(0u32, |acc, (node, node_next)| {
+                    acc + graph
+                        .edges_connecting(*node, *node_next)
+                        .next()
+                        .unwrap()
+                        .weight()
+                })
+        })
+        .max()
+        .unwrap();
 
-pub fn longest_path<G, F, H, K, IsGoal>(
-    graph: G,
-    start: G::NodeId,
-    mut is_goal: IsGoal,
-    mut edge_cost: F,
-    mut estimate_cost: H,
-) -> Option<K>
-where
-    G: IntoEdges + Visitable,
-    IsGoal: FnMut(G::NodeId) -> bool,
-    G::NodeId: Eq + Hash,
-    F: FnMut(G::EdgeRef) -> K,
-    H: FnMut(G::NodeId) -> K,
-    K: Measure + Copy,
-{
-    let mut visit_next = BinaryHeap::new();
-    let mut scores = HashMap::new(); // g-values, cost to reach the node
-    let mut estimate_scores = HashMap::new(); // f-values, cost to reach + estimate cost to goal
-
-    let zero_score = K::default();
-    scores.insert(start, zero_score);
-    visit_next.push(MaxScored(estimate_cost(start), start));
-
-    while let Some(MaxScored(estimate_score, node)) = visit_next.pop() {
-        if is_goal(node) {
-            let cost = scores[&node];
-            return Some(cost);
-        }
-
-        // This lookup can be unwrapped without fear of panic since the node was necessarily scored
-        // before adding it to `visit_next`.
-        let node_score = scores[&node];
-
-        match estimate_scores.entry(node) {
-            Occupied(mut entry) => {
-                // If the node has already been visited with an equal or lower score than now, then
-                // we do not need to re-visit it.
-                if *entry.get() <= estimate_score {
-                    continue;
-                }
-                entry.insert(estimate_score);
-            }
-            Vacant(entry) => {
-                entry.insert(estimate_score);
-            }
-        }
-
-        for edge in graph.edges(node) {
-            let next = edge.target();
-            let next_score = node_score + edge_cost(edge);
-
-            match scores.entry(next) {
-                Occupied(mut entry) => {
-                    // No need to add neighbors that we have already reached through a shorter path
-                    // than now.
-                    if *entry.get() <= next_score {
-                        continue;
-                    }
-                    entry.insert(next_score);
-                }
-                Vacant(entry) => {
-                    entry.insert(next_score);
-                }
-            }
-
-            let next_estimate_score = next_score + estimate_cost(next);
-            visit_next.push(MaxScored(next_estimate_score, next));
-        }
-    }
-    None
+    longest.to_string()
 }
 
 #[derive(Debug)]
@@ -340,48 +268,6 @@ struct GardenNode {
     position: IVec2,
     graph_idx: Option<NodeIndex>,
     tread: Tread,
-}
-
-#[derive(Debug, Clone, Copy)]
-struct MaxScored<K, T>(K, T);
-
-impl<K: PartialOrd, T> PartialEq for MaxScored<K, T> {
-    #[inline]
-    fn eq(&self, other: &MaxScored<K, T>) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-
-impl<K: PartialOrd, T> Eq for MaxScored<K, T> {}
-
-impl<K: PartialOrd, T> PartialOrd for MaxScored<K, T> {
-    #[inline]
-    fn partial_cmp(&self, other: &MaxScored<K, T>) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<K: PartialOrd, T> Ord for MaxScored<K, T> {
-    #[inline]
-    fn cmp(&self, other: &MaxScored<K, T>) -> Ordering {
-        let a = &self.0;
-        let b = &other.0;
-        if a == b {
-            Ordering::Equal
-        } else if a < b {
-            Ordering::Less
-        } else if a > b {
-            Ordering::Greater
-        } else if a.ne(a) && b.ne(b) {
-            // these are the NaN cases
-            Ordering::Equal
-        } else if a.ne(a) {
-            // Order NaN less, so that it is last in the MinScore order
-            Ordering::Greater
-        } else {
-            Ordering::Less
-        }
-    }
 }
 
 #[cfg(test)]
